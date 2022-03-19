ed85f2ce8c080cad18511585e6f831eeb6e4e4bd
Andreia Cifoni <andreiacifoni@gmail.com>
2022-03-19 15:07:45 -0400

    validation

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -38,6 +38,7 @@ router.post(
     //   throw new ExpressError("Invalid Campground Data", 400);
 
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash("success", "Successfully made a new campground!");
     res.redirect(`/campgrounds/${campground._id}`);
@@ -47,9 +48,10 @@ router.post(
 router.get(
   "/:id",
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate(
-      "reviews"
-    );
+    const campground = await Campground.findById(req.params.id)
+      .populate("reviews")
+      .populate("author");
+    console.log(campground);
     if (!campground) {
       req.flash("error", "Cannot find that campground!");
       return res.redirect("/campgrounds");
@@ -77,6 +79,7 @@ router.put(
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
+    const campground = await Campground.findById(id);
     const campground = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
