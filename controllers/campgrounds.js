355d4c44992e4d4f0d5b8c8cc657bf621e6cbb64
Andreia Cifoni <andreiacifoni@gmail.com>
2022-03-23 18:02:16 -0400

    deleting image from database and cloudinary

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -47,6 +47,7 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
   const { id } = req.params;
+  console.log(req.body);
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
@@ -56,6 +57,15 @@ module.exports.updateCampground = async (req, res) => {
   }));
   campground.images.push(...imgs);
   await campground.save();
+  if (req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
+    }
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+    console.log(campground);
+  }
   req.flash("success", "Successfully updated Campground!");
   res.redirect(`/campgrounds/${campground._id}`);
 };
