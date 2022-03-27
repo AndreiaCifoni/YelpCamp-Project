3157d19fa4205324215e6cd96859c4d78c324e07
Andreia Cifoni <andreiacifoni@gmail.com>
2022-03-27 11:03:24 -0400

    sanitaze HTML

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
+const mongoSanitize = require("express-mongo-sanitize");
+
 const userRoutes = require("./routes/users");
 const campgroundsRoutes = require("./routes/campgrounds");
 const reviewsRoutes = require("./routes/reviews");
@@ -38,8 +40,10 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
 const sessionConfig = {
+  name: "session",
   secret: "thissholdbeabettersecret",
   resave: false,
   saveUninitialized: true,
@@ -60,17 +64,10 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-  console.log(req.session);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
-});
-
-app.get("/fakeUser", async (req, res) => {
-  const user = new User({ email: "andreia@gmail.com", username: "deia" });
-  const newUser = await User.register(user, "abc");
-  res.send(newUser);
 });
 
 app.use("/", userRoutes);
